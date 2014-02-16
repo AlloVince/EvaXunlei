9d7b5dc3f2076a8ee706987a8ec1f56abd9f3d6f
AlloVince <allo.vince@gmail.com>
2014-02-16 23:01:19 +0800

    add adapter from sobt

--- index.php
+++ index.php
@@ -17,72 +17,93 @@ function toJSON($results)
     echo $results;
 }
 
-$keyword = isset($_GET['q']) ? $_GET['q'] : '';
-$response = '';
-if(!$keyword) {
-    header('Location:http://avnpc.com/', false, 301);
-} else {
-    $cache = StorageFactory::factory(array(
-        'adapter' => array(
-            'name' => 'filesystem',
-            'options' => array(
-                'ttl' => 3600 * 24,
-                'dirLevel' => 2,
-                'cacheDir' => __DIR__ . '/cache',
-                'dirPermission' => 0755,
-                'filePermission' => 0666,
-                'namespaceSeparator' => 'evaxunlei_'
-            ),
-        ),
-        'plugins' => array(
-            'exception_handler' => array(
-                'throw_exceptions' => true
-            ),
-            'serializer'
-        )
+function getResultsFromSobt($keyword)
+{
+    $res = \Requests::get('http://so-bt.com/web.php?' . http_build_query(array(
+        'q' => $keyword,
+    )), array(
+        'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0',
+        'Accept' => 'text/html',
+        'Accept-Language' => 'zh-cn,zh;q=0.8,ja;q=0.6,en-us;q=0.4,en;q=0.2',
+        'Accept-Encoding' => 'gzip, deflate',
+    ), array(
     ));
-    $cacheKey = md5($keyword);
-    if (!$results = $cache->getItem($cacheKey)) {
-        $res = \Requests::get('http://btdigg.org/search?' . http_build_query(array(
-            'q' => $keyword,
-        )), array(
-            'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0',
-            'Accept' => 'text/html',
-            'Accept-Language' => 'zh-cn,zh;q=0.8,ja;q=0.6,en-us;q=0.4,en;q=0.2',
-            'Accept-Encoding' => 'gzip, deflate',
-        ), array(
-            'timeout' => 100000
-        ));
 
-        $links = array();
-        if($res->status_code == '200') {
-            $dom = HtmlDomParser::str_get_html( $res->body );
-            foreach($dom->find('#search_res tr') as $li) {
-                $titleLink = $li->find('.torrent_name a', 0);
-                $metaLink = $li->find('.ttth a', 0);
-                if(!$titleLink || !$metaLink) {
-                    continue;
-                }
-                $hash = preg_match('/btih:(\w+)/', $metaLink->href, $match);
-                $hash = strtoupper($match[1]);
-                $redirect = 'http://vod.xunlei.com/share.html?url=magnet%3A%3Fxt%3Durn%3Abtih%3A' . $hash;
-                $links[] = array(
-                    'title' => strip_tags($titleLink->innertext),
-                    'hash' => $hash,
-                    'magnet' => html_entity_decode($metaLink->href),
-                    'redirect' => 'http://allovince.xunlei.com/redirect.html?' . http_build_query(array(
-                        'url' => $redirect
-                    )),
-                );
+    $links = array();
+    if($res->status_code == '200') {
+        $dom = HtmlDomParser::str_get_html( $res->body );
+        foreach($dom->find('.content li') as $li) {
+            $titleLink = $li->find('.title', 0);
+            $metaLink = $li->find('.meta a', 0);
+            if(!$titleLink || !$metaLink) {
+                continue;
             }
+            $hash = preg_match('/btih:(\w+)/', $metaLink->href, $match);
+            $hash = strtoupper($match[1]);
+            $redirect = 'http://vod.xunlei.com/share.html?url=magnet%3A%3Fxt%3Durn%3Abtih%3A' . $hash;
+            $links[] = array(
+                'title' => strip_tags($titleLink->innertext),
+                'hash' => $hash,
+                'magnet' => html_entity_decode($metaLink->href),
+                'redirect' => 'http://allovince.xunlei.com/redirect.html?' . http_build_query(array(
+                    'url' => $redirect
+                )),
+            );
         }
-        $results = array(
-            'count' => count($links),
-            'results' => $links,
-        );
-        $cache->setItem($cacheKey, $results);
     }
-    /*
+    $results = array(
+        'count' => count($links),
+        'results' => $links,
+    );
+
+    return $results;
+}
+
+function getResultsFromBtdigg($keyword)
+{
+    $res = \Requests::get('http://btdigg.org/search?' . http_build_query(array(
+        'q' => $keyword,
+    )), array(
+        'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0',
+        'Accept' => 'text/html',
+        'Accept-Language' => 'zh-cn,zh;q=0.8,ja;q=0.6,en-us;q=0.4,en;q=0.2',
+        'Accept-Encoding' => 'gzip, deflate',
+    ), array(
+        'timeout' => 100000
+    ));
+
+    $links = array();
+    if($res->status_code == '200') {
+        $dom = HtmlDomParser::str_get_html( $res->body );
+        foreach($dom->find('#search_res tr') as $li) {
+            $titleLink = $li->find('.torrent_name a', 0);
+            $metaLink = $li->find('.ttth a', 0);
+            if(!$titleLink || !$metaLink) {
+                continue;
+            }
+            $hash = preg_match('/btih:(\w+)/', $metaLink->href, $match);
+            $hash = strtoupper($match[1]);
+            $redirect = 'http://vod.xunlei.com/share.html?url=magnet%3A%3Fxt%3Durn%3Abtih%3A' . $hash;
+            $links[] = array(
+                'title' => strip_tags($titleLink->innertext),
+                'hash' => $hash,
+                'magnet' => html_entity_decode($metaLink->href),
+                'redirect' => 'http://allovince.xunlei.com/redirect.html?' . http_build_query(array(
+                    'url' => $redirect
+                )),
+            );
+        }
+    }
+    $results = array(
+        'count' => count($links),
+        'results' => $links,
+    );
+
+    return $results;
+}
+
+function getResultsFromShousibaocai()
+{
     $res = \Requests::get('http://bt.shousibaocai.com/?' . http_build_query(array(
         's' => $keyword,
     )), array(
@@ -109,12 +130,50 @@ if(!$keyword) {
             $links[] = array(
                 'title' => strip_tags($titleLink->innertext),
                 'hash' => $hash,
+                'magnet' => html_entity_decode($metaLink->href),
                 'redirect' => 'http://allovince.xunlei.com/redirect.html?' . http_build_query(array(
                     'url' => $redirect
                 )),
             );
         }
     }
-    */
+    $results = array(
+        'count' => count($links),
+        'results' => $links,
+    );
+
+    return $results;
+}
+
+$keyword = isset($_GET['q']) ? $_GET['q'] : '';
+$response = '';
+if(!$keyword) {
+    header('Location:http://avnpc.com/', false, 301);
+} else {
+    $cache = StorageFactory::factory(array(
+        'adapter' => array(
+            'name' => 'filesystem',
+            'options' => array(
+                'ttl' => 3600 * 24,
+                'dirLevel' => 2,
+                'cacheDir' => __DIR__ . '/cache',
+                'dirPermission' => 0755,
+                'filePermission' => 0666,
+                'namespaceSeparator' => 'evaxunlei_'
+            ),
+        ),
+        'plugins' => array(
+            'exception_handler' => array(
+                'throw_exceptions' => true
+            ),
+            'serializer'
+        )
+    ));
+    $cacheKey = md5($keyword);
+    if (!$results = $cache->getItem($cacheKey)) {
+        //$results = getResultsFromBtdigg($keyword);
+        $results = getResultsFromSobt($keyword);
+        $cache->setItem($cacheKey, $results);
+    }
     toJSON($results);
 }
